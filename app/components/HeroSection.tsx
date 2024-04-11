9c095a228fed34ce026bcb486bfa72237c0f730b
hayzedd2 <azeezalhameen1@gmail.com>
2024-04-11 14:26:26 +0100

    add about

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,7 +1,14 @@
+'use client'
+
 import React from "react";
 import { Open_Sans } from "next/font/google";
 const open_Sans = Open_Sans({ subsets: ["latin"], style: "italic" });
+import { useState } from "react";
 const HeroSection = () => {
+  const [open , setOpen] = useState(false)
+  const onOpen = ()=>{
+    setOpen(!open)
+  }
   return (
     <section>
       <div className="pt-16">
@@ -10,14 +17,21 @@ const HeroSection = () => {
         </div>
         <div className="xl:max-w-[35rem] sm:max-w-none mt-[14px] ">
           <p className="leading-[30px]">
-            Hi there , I am 
+            Hi there , I am
             <span className=" bg-[#ebecfe] mx-1 font-[600] text-[#7457df] py-[0.1rem] text-[0.9rem] px-[0.5rem] rounded-[0.5rem]">
-               Azeez Alhameen 
+              Azeez Alhameen
             </span>
             . I&apos;m a creative frontend developer with a zeal for translating
-            ideas into beautiful, functional websites, I&apos;m  dedicated to
-            enhancing user experiences through code.
+            ideas into beautiful, functional web-applications.
           </p>
+          <p className="mt-4 leading-[30px]">
+            Pushed by my curiosity ,I&apos;ve always loved building things . I
+            enjoy crafting user-centered interface through code . {!open ?  <span onClick={onOpen} className=" cursor-pointer ml-[0.15rem] text-[0.85rem] underline underline-offset-2">Read more?</span> : null}
+          </p>
+          {open ? <p className="mt-4 leading-[30px]">
+          What sets me apart as a frontend developer is my commitment to user-centered design and my ability to collaborate effectively within cross-functional teams. When you bring me onto your project, you gain a collaborator who is not only a skilled coder but also a creative problem-solver. My goal is to turn your vision into reality and help your project reach new heights. <span onClick={onOpen} className=" cursor-pointer ml-[0.15rem] text-[0.85rem] underline underline-offset-2">Read less</span>
+          </p> : null}
+          
         </div>
       </div>
     </section>
--- app/components/Technologies.tsx
+++ app/components/Technologies.tsx
@@ -6,6 +6,7 @@ import { TbBrandNextjs } from "react-icons/tb";
 import { GrCss3 } from "react-icons/gr";
 import { RiJavascriptLine } from "react-icons/ri";
 import { TbBrandTailwind } from "react-icons/tb";
+import { BiLogoTailwindCss } from "react-icons/bi";
 import { TbBrandPrisma } from "react-icons/tb";
 import { TbBrandDjango } from "react-icons/tb";
 
@@ -14,47 +15,49 @@ const Technologies = () => {
     <section>
       <div className="heading pt-10">
         <h1 className="text-[0.85rem] py-1 bg-[#1b1c1d] tech gap-[0.18rem] w-[7rem] rounded-[5px] flex items-center justify-center">
-          Technologies <VscTriangleRight className=" font-[700] " />{" "}
+          Technologies <VscTriangleRight className=" font-[700] " />
         </h1>
+        <p className="py-2 font-[500] underline underline-offset-2">
+          Some of the Technologies i work with
+        </p>
       </div>
-      <div className="my-5 xl:w-[35vw] sm:w-full skills rounded-[0.9rem] flex">
+      <div className="mb-5 mt-3 xl:w-[35vw] sm:w-full skills rounded-[0.9rem] flex">
         <div className="col-1 border-r-[#d9d9d9] border-r basis-[50%]">
           <p className="flex gap-1 items-center text-black font-[500] xl:px-10 sm:px-6 py-[1.3rem]  border-b-[#d9d9d9] border-b">
-            <TbBrandHtml5 className="text-[1.4rem] " /> Html5
+            <TbBrandHtml5 className="text-[1.3rem] " /> Html5
           </p>
-          <p className="flex gap-1 items-center  text-black font-[500]  xl:px-10 sm:px-6 py-[1.3rem] border-b-[#d9d9d9] border-b ">
-            <TbBrandTypescript className="text-[1.25rem]" />
-            Typescript
-          </p>
+          
           <p className="flex gap-1 items-center text-black font-[500]  xl:px-10 sm:px-6 py-[1.3rem] border-b-[#d9d9d9] border-b ">
-            <RiJavascriptLine className="text-[1.4rem] " />
+            <RiJavascriptLine className="text-[1.35rem] " />
             Javascript
           </p>
+          <p className="flex gap-1 items-center text-black font-[500] xl:px-10 sm:px-6 py-[1.3rem]  border-b-[#d9d9d9] border-b">
+            <TbBrandNextjs className="text-[1.4rem] " />
+            Nextjs
+          </p>
           <p className="flex gap-1 items-center  text-black font-[500] xl:px-10 sm:px-6 py-[1.3rem] ">
-            <TbBrandDjango className="text-[1.25rem]" />
+            <TbBrandDjango className="text-[1.3rem]" />
             Django
           </p>
-         
         </div>
         <div className="col-1 basis-[50%]">
           <p className="flex gap-1 items-center  text-black font-[500] xl:px-10 sm:px-6 py-[1.3rem]  border-b-[#d9d9d9] border-b">
             <GrCss3 className="text-[1.25rem]" />
             Css3
           </p>
-          <p className="flex gap-1 items-center text-black font-[500] xl:px-10 sm:px-6 py-[1.3rem]  border-b-[#d9d9d9] border-b">
-            <TbBrandNextjs className="text-[1.4rem] " />
-            Nextjs
+          <p className="flex gap-1 items-center  text-black font-[500]  xl:px-10 sm:px-6 py-[1.3rem] border-b-[#d9d9d9] border-b ">
+            <TbBrandTypescript className="text-[1.3rem]" />
+            Typescript
           </p>
+          
           <p className="flex gap-1 items-center  text-black font-[500] xl:px-10 sm:px-6 py-[1.3rem] border-b-[#d9d9d9] border-b  ">
-            <TbBrandTailwind className="text-[1.25rem]" />
+            <BiLogoTailwindCss className="text-[1.3rem]" />
             Tailwind-Css
           </p>
           <p className="flex gap-1 items-center text-black font-[500]  xl:px-10 sm:px-6 py-[1.3rem] ">
             <TbBrandPrisma className="text-[1.3rem] " />
             Prisma
           </p>
-         
-         
         </div>
       </div>
     </section>
