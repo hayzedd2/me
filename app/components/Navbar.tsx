0675df46f98ec750f63955d11f0faa32c5215fe4
hayzedd2 <azeezalhameen1@gmail.com>
2024-04-12 20:46:38 +0100

    btn animation

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,17 +5,17 @@ const josefin = Josefin_Sans({ subsets: ["latin"] });
 
 const Navbar = () => {
   return (
-    <nav className="w-full flex items-center justify-between">
+    <nav className="min-w-[55rem] fixed  bg-white flex items-center justify-between">
       <div className={josefin.className}>
         <h1 className="text-[1.7rem] font-[600]">Az</h1>
       </div>
 
-      <div className="bg-[#1b1c1d] hover:translate-y-[-2px] cursor-pointer transition-all p-1 rounded-[50px] text-[0.75rem] work">
+      <div className="bg-[#1b1c1d] shiny-button hover:translate-y-[-2px] cursor-pointer transition-all p-1 rounded-[50px] text-[0.75rem] work">
         <a
           href="mailto:azeezalhameen@gmail.com"
           className="flex items-center gap-1"
         >
-          Available for work{" "}
+          Available for work
           <FaAngleRight className="text-[#99999a] font-[700] " />
         </a>
       </div>
--- app/components/Toolbar.tsx
+++ app/components/Toolbar.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import { FaGithub } from "react-icons/fa";
 import { IoLogoLinkedin } from "react-icons/io";
 import { IoMail } from "react-icons/io5";
-import { CgMail } from "react-icons/cg";
 
 const Toolbar = () => {
   return (
-    <nav className='w-full flex xl:items-center sm:items-end xl:justify-center sm:justify-end'>
+    <nav className='w-full flex items-end justify-end'>
         <div  className="fixed xl:w-44 sm:w-[9.5rem] h-[3.2rem] bg-white flex text-[1.2rem] xl:gap-[1.35rem] sm:gap-[1.2rem]  items-center justify-center toolbar rounded-[7000px] sm:bottom-6 xl:bottom-10">
             <a href=""><FaGithub/></a>
             <a href=""><IoLogoLinkedin className='text-[1.5rem]'/></a>
