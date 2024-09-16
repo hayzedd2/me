2609153c5bbb0ad9fba4397f9b4eb7679f87caac
hayzedd2 <azeezalhameen1@gmail.com>
2024-09-16 20:38:46 +0100

    links animation

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <Projects />
         <Respositories />
         <Contact />
-        <Toolbar />
+        {/* <Toolbar /> */}
       </main>
     </section>
   );
