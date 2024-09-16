528801674f39545227fa6c22895577089b1be8c6
hayzedd2 <azeezalhameen1@gmail.com>
2024-09-16 19:08:31 +0100

    toolbar changed

--- app/components/Toolbar.tsx
+++ app/components/Toolbar.tsx
@@ -8,12 +8,12 @@ import { VscGithub, VscGithubAlt } from 'react-icons/vsc';
 const Toolbar = () => {
   return (
     <nav className='w-full flex items-end justify-end'>
-        <div  className="fixed xl:w-42 sm:w-[9.5rem] h-[3.2rem] bg-white flex text-[1.2rem] xl:gap-[1.2rem] sm:gap-[1.2rem]  items-center justify-center  bx-shadow-light rounded-[7000px] sm:bottom-6 xl:bottom-10">
-            <a href="https://github.com/hayzedd2/"><VscGithubAlt/></a>
-            <a href="http://linkedin.com/in/azeez-alhameen-9a604026a"><PiLinkedinLogo className='text-[1.3rem]'/></a>
-            <a href="mailto:azeezalhameen1@gmail.com"><CiMail className='text-[1.4rem] text-black'/></a>
-        </div>
-        
+      <div className="fixed w-auto bg-white flex text-[0.85rem] px-3 py-1 gap-[1.1rem]  items-center justify-center  bx-shadow-light rounded-[7000px] sm:bottom-6 xl:bottom-10">
+        <a href="https://github.com/hayzedd2/">Github</a>
+        <a href="http://linkedin.com/in/azeez-alhameen-9a604026a">Linkedin</a>
+        <a href="mailto:azeezalhameen1@gmail.com">Mail</a>
+      </div>
+
     </nav>
   )
 }
